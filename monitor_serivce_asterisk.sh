f9ac11a41525f86fd6295a2444bb291e73001c24
root <root@localhost.localdomain>
2018-09-04 11:59:21 +0700

    monitor service asterisk on tool PRTG via ssh

--- monitor_serivce_asterisk.sh
+++ monitor_serivce_asterisk.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 serviceIsRunning=false
-var1=$(/etc/init.d/asterisk status | awk '{print $5}')
+	var1=$(/etc/init.d/asterisk status | awk '{print $5}')
 if [ "$var1" == 'running...' ]
-then
-serviceIsRunning=true
-echo "0:200:Running"
-fi
-if [ $serviceIsRunning == false ]
-then
-echo "2:404:Stop"
-fi
+	then
+		serviceIsRunning=true
+		echo "0:200:Running"
+	fi
+	if [ $serviceIsRunning == false ]
+		then
+		echo "2:404:Stop"
+	fi
 
